4ae377464e619801300e5da5aa62730a44817660
jkmizz <kohler@smith@gmail.com>
2017-07-22 16:47:30 -0500

    Modified template to display posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Recipe
 
 def recipe_list(request):
-    return render(request, 'blog/recipe_list.html', {})
+	recipes = Recipe.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/recipe_list.html', {'recipes':recipes})
 # Create your views here.
+
+
